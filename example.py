9bddd3e45ec94688c7fcba86c9ef62ba495ee1d1
leafcoder <leafcoder@gmail.com>
2017-06-28 10:56:46 +0800

    修改端口地址

--- example.py
+++ example.py
@@ -5,6 +5,6 @@ sys.dont_write_bytecode = True
 
 import litefs
 litefs = litefs.Litefs(
-    address='localhost:9090', webroot='./site', debug=True
+    address='localhost:8080', webroot='./site', debug=True
 )
 litefs.run(timeout=2.)
