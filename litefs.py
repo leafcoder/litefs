8678ad25bc680763cd2d3979a5f97d45d4fce00f
leafcoder <leafcoder@gmail.com>
2018-03-01 09:02:15 +0800

    解决使用pypi安装时发生错误的问题

--- litefs.py
+++ litefs.py
@@ -1,21 +1,17 @@
 #-*- coding: utf-8 -*-
 
-'''
-使用 Python 从零开始构建一个 Web 服务器框架。 开发 Litefs 的是为了实现一个能快速、\
-安全、灵活的构建 Web 项目的服务器框架。 litefs 是一个高性能的 Http 服务器。Litefs 具\
-有高稳定性、丰富的功能、系统消耗低的特点。
+'''使用 Python 从零开始构建一个 Web 服务器框架。 开发 Litefs 的是为了实现一个能快速、安\
+全、灵活的构建 Web 项目的服务器框架。 Litefs 是一个高性能的 HTTP 服务器。Litefs 具有高\
+稳定性、丰富的功能、系统消耗低的特点。
 
-Name: leozhang
+Name: leafcoder
 Email: leafcoder@gmail.com
 
 Copyright (c) 2017, Leafcoder.
 License: MIT (see LICENSE for details)
 '''
 
-version_major = 0
-version_minor = 2
-version_build = 0
-__version__ = '%s.%s.%s' % (version_major, version_minor, version_build)
+__version__ = '0.2.0'
 __author__  = 'Leafcoder'
 __license__ = 'MIT'
 
