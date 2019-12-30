4063598398710e55065029eb76bdee7a2cec2ea1
leafcoder <leafcoder@gmail.com>
2019-12-30 14:32:06 +0800

    fix: change example.py

--- example.py
+++ example.py
@@ -2,13 +2,15 @@
 
 import sys
 sys.dont_write_bytecode = True
-sys.path.insert(0, '..')
 
 import litefs
-port = 8080
-if len(sys.argv) > 1:
+try:
     port = int(sys.argv[1])
+except IndexError:
+    port = 9090
 litefs = litefs.Litefs(
-    address='0.0.0.0:%s' % port, webroot='./site', debug=True
+    address='0.0.0.0:%s' % port,
+    webroot='./site',
+    debug=True
 )
 litefs.run(timeout=2.)
